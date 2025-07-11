c8d1c9f2de36ae1a812f40008a09e9b32b8e1806
HYELIM KIM <hl0121@naver.com>
2025-07-11 16:50:04 +0900

    update

--- 1-4/design_dome.py
+++ 1-4/design_dome.py
@@ -20,17 +20,17 @@ material_density = {
 GRAVITY_CORRECTION = 0.38
 
 def sphere_area(diameter, material='유리', thickness=1):
-    radius_cm = (diameter * 100) / 2  # m -> cm
-    area = 2 * 3.141592 * radius_cm ** 2
-    volume = area * thickness
-    density = material_density.get(material, 2.4)
-    weight = volume * density * GRAVITY_CORRECTION / 1000
-
+    radius_cm = (diameter * 100) / 2  # 반지름 계산 m -> cm (입력된 지름을 cm 단위로 변환하고 2로 나눠서 반지름(cm)을 구합니다.)
+    area = 2 * 3.141592 * radius_cm ** 2 # 반구의 겉면적 계산 (반구의 겉면적 = 2 * π * r²), 3.141592는 파이(π)의 근삿값
+    volume = area * thickness # 재료 부피 계산 (부피 cm³ = 면적 * 두께)
+    density = material_density.get(material, 2.4) #밀도값 가져오기, material_density 딕셔너리에서 재료 이름에 맞는 밀도를 가져옴, 해당 재질이 없으면 기본값 2.4(유리)를 사용
+    weight = volume * density * GRAVITY_CORRECTION / 1000 #무게 계산 (질량 = 부피 cm³ * 밀도 g/cm³), (무게 = 질량 * 중력 보정 / 1000), (/ 1000은 단위 변환 (g → kg))
+    # 전역 변수에 결과 저장
     result['재질'] = material
     result['지름'] = diameter
     result['두께'] = thickness
-    result['면적'] = round(area, 3)
-    result['무게'] = round(weight, 3)
+    result['면적'] = round(area, 3) # 면적을 소수점 이하 3자리까지 반올림
+    result['무게'] = round(weight, 3) # 무게를 소수점 이하 3자리까지 반올림
 
     print(f"재질 ⇒ {result['재질']}, 지름 ⇒ {result['지름']}m, 두께 ⇒ {result['두께']}cm, 면적 ⇒ {result['면적']}cm², 무게 ⇒ {result['무게']}kg")
 
